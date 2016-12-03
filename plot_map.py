8803a476d3ed6158d114198404a652339cebf155
hzhao16 <econzh@gmail.com>
2016-12-03 01:54:23 -0500

    plot spots on Google map

---
+++ plot_map.py
@@ -0,0 +1,26 @@
+import codecs
+
+
+def plot_map(f):
+	'''f is the dataframe(sorted nearby locations) with lat & lng'''
+	fhand = codecs.open('wheref.js','w', "utf-8")
+	fhand.write("myData = [\n")
+	count = 0
+	output= []
+
+	for i in range(f.shape[0]):
+    
+    	lat = f['lat'].iloc[i]
+    	lng = f['lng'].iloc[i]
+    	name = f['name'].iloc[i]
+    	address = f['address'].iloc[i].strip()
+    	try:
+        	count += 1
+        	if count > 1 : fhand.write(",\n")
+        	output = "["+str(lat)+","+str(lng)+", \""+name+"\", "+ "\""+str(address)+"\"]"
+        	fhand.write(output)
+    	except:
+        	continue
+
+	fhand.write("\n];\n")
+	fhand.close()
